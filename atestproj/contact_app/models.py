f8e0d5e1ee4e5a79fa3107157d1fa855d524b6bb
Buhle Ntshalintshali <buhlentshalintshali@Buhles-MBP.yookos.local>
2015-05-19 09:59:57 +0200

    added gender on models

--- atestproj/contact_app/models.py
+++ atestproj/contact_app/models.py
@@ -5,4 +5,8 @@ from django.db import models
 class Contact(models.Model):
 	firstname 	= models.CharField(max_length=25)
 	lastname 	= models.CharField(max_length=25)
+	gender		= models.CharField(max_length=10)
+
+#class Details(models.Model):
+
 
