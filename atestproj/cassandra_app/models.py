123a7b63ae53358d59a1e0b9de5f0f65608545bf
= <=>
2015-05-20 13:12:09 +0200

    Adding the cassandra app on this project

---
+++ atestproj/cassandra_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+import uuid
+from cassandra.cqlengine import columns
+from cassandra.cqlengine.models import Model
+
+class ExampleModel(Model):
+	read_repair_chance = 0.5
+	example_id 		= columns.UUID(primary_key=True, default=uuid.uuid4)
+	example_type	= columns.Integer(index=True)
+	created_at		= columns.DateTime()
+	description		= columns.Text(required=False)
